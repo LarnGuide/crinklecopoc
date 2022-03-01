b7e9f3309126334474e95cbe4a747094c94dafae
user <user@email.com>
2022-03-01 10:15:41 +0000

    deploy

---
+++ precache-manifest.9e579609f4870d4840ba18695c3f35ff.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf27b1d4eabec8852c97d116abb06ec",
+    "url": "/crinklecopoc/404.html"
+  },
+  {
+    "revision": "10c7497451338e39ebdee3e300f3f6ff",
+    "url": "/crinklecopoc/controllers/HtmlSanitizer.js"
+  },
+  {
+    "revision": "57bc10a3c5d83ae73d9a",
+    "url": "/crinklecopoc/css/chunk-vendors.4ead101a.css"
+  },
+  {
+    "revision": "70d02da3d3ede20af8d9",
+    "url": "/crinklecopoc/css/index.8d68b6a8.css"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/crinklecopoc/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/crinklecopoc/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/crinklecopoc/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/crinklecopoc/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/crinklecopoc/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/crinklecopoc/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "e04efa7527460e6d38b1ba79b405761b",
+    "url": "/crinklecopoc/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.e04efa75.woff"
+  },
+  {
+    "revision": "3e82d9a3cad4ab32b96d5589d55db0e6",
+    "url": "/crinklecopoc/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.3e82d9a3.woff2"
+  },
+  {
+    "revision": "3440db03f1a473c8ad5f2e6c308d1d89",
+    "url": "/crinklecopoc/img/splashscreens/ipad_splash.png"
+  },
+  {
+    "revision": "fc7721d9857fd8f0aebd48d965a4cf62",
+    "url": "/crinklecopoc/img/splashscreens/ipadpro1_splash.png"
+  },
+  {
+    "revision": "1d9d8354c0d4d409865a6a3bf29f92ae",
+    "url": "/crinklecopoc/img/splashscreens/ipadpro2_splash.png"
+  },
+  {
+    "revision": "b09ff2c15277b3bc985b76583c9a6fd6",
+    "url": "/crinklecopoc/img/splashscreens/ipadpro3_splash.png"
+  },
+  {
+    "revision": "66550973597309f00a6156039ffed25f",
+    "url": "/crinklecopoc/img/splashscreens/iphone5_splash.png"
+  },
+  {
+    "revision": "b8b57de79986ac5498a27d680be548fb",
+    "url": "/crinklecopoc/img/splashscreens/iphone6_splash.png"
+  },
+  {
+    "revision": "9d973775ce00cf29887a5d9d1ba334f5",
+    "url": "/crinklecopoc/img/splashscreens/iphoneplus_splash.png"
+  },
+  {
+    "revision": "3d89a57182fada7c3c31af9cb39185d1",
+    "url": "/crinklecopoc/img/splashscreens/iphonex_splash.png"
+  },
+  {
+    "revision": "a0ecab7ecebbab92dc60867e4193b976",
+    "url": "/crinklecopoc/img/splashscreens/iphonexr_splash.png"
+  },
+  {
+    "revision": "013f081727eafc6533e910a66bf20b38",
+    "url": "/crinklecopoc/img/splashscreens/iphonexsmax_splash.png"
+  },
+  {
+    "revision": "4229aa532128082cc9b60daa977178cf",
+    "url": "/crinklecopoc/index.html"
+  },
+  {
+    "revision": "57bc10a3c5d83ae73d9a",
+    "url": "/crinklecopoc/js/chunk-vendors.9f17187e.js"
+  },
+  {
+    "revision": "70d02da3d3ede20af8d9",
+    "url": "/crinklecopoc/js/index.46f14632.js"
+  },
+  {
+    "revision": "e61d8b9adf13131025f1548e9f54505b",
+    "url": "/crinklecopoc/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crinklecopoc/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/crinklecopoc/precache-manifest.9e579609f4870d4840ba18695c3f35ff.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "CrinkleCo"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
